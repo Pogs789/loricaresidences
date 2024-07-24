b04ae382e1acb4d7fa668bd1a2b7d301725ad3a4
Pogs789 <cesphillipmiraflor.lorica@bicol-u.edu.ph>
2024-07-24 15:06:25 +0800

    changed the name from verify_otp to otp to prevent confusion

--- controller/verify_otp.php
+++ controller/verify_otp.php
@@ -1,28 +1,31 @@
 <?php
 session_start();
-require_once('connector.php');
+require_once "connector.php";
 
-if (!empty($_POST["otp"])) {
-    $otp = htmlspecialchars($_POST["otp"]);
-    $stmt = $conn->prepare("SELECT * FROM otp_verification WHERE otp = ? AND is_expired = 0 AND NOW() <= DATE_ADD(created_at, INTERVAL 24 HOUR)");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST['otp'])) {
+        header("Location: ../verify_otp.php?error=empty");
+        exit();
+    }
+
+    $otp = intval($_POST['otp']);
+
+    $stmt = $conn->prepare("SELECT * FROM otp_verification WHERE otp = ? AND is_expired != 1 AND NOW() <= DATE_ADD(created_at, INTERVAL 24 HOUR)");
     $stmt->bind_param('i', $otp);
     $stmt->execute();
     $stmt->store_result();
 
-    if ($stmt->num_rows > 0) {
+    if ($stmt->num_rows == 1) {
         $stmt2 = $conn->prepare("UPDATE otp_verification SET is_expired = 1 WHERE otp = ?");
         $stmt2->bind_param('i', $otp);
         $stmt2->execute();
-        $stmt2->close();
         header("Location: ../tenant/dashboard.php");
         exit();
     } else {
-        $_SESSION['error_message'] = "The OTP you entered is invalid. Please try again.";
-        header("Location: ../login.php?otp=required");
+        header("Location: ../verify_otp.php?error=invalid");
         exit();
     }
 } else {
-    $_SESSION['error_message'] = "Please enter the OTP.";
-    header("Location: ../login.php?otp=required");
+    header("Location: ../verify_otp.php?error=unknown");
     exit();
 }
